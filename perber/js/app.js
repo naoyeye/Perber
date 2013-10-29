d49bf71a0ea8340bea65c5ae954854a8d102dd94
J.Y Han <ihanjiyun@gmail.com>
2013-10-30 01:05:09 +0800

    modify google analytics

--- perber/js/app.js
+++ perber/js/app.js
@@ -5,32 +5,47 @@ App = new Backbone.Marionette.Application();
 // add some Regions
 App.addRegions({
     // drawRegion: Perber.Region.Draw,
-    mainRegion: Perber.Region.Main,
-    headerRegion: Perber.Region.Header,
+    mainRegion: '#main',
+    headerRegion: '#header',
     toolbarRegion:'#toolbar',
     sidebarRegion:'#sidebar',
+    // videosRegion: "#videos",
     // overlayRegion: "#overlay",
     // loadingRegion: "#loadingWave",
-    modalRegion:Perber.Region.Modal,
+    // modalRegion:Perber.Region.Modal,
 });
 
 App.addInitializer(function(){
+    Perber.user = new Perber.Model.Me(Perber.config.user);
+    
+    Perber.user.start();
+    Perber.users = new Perber.Collection.Users();
+
+    Perber.activity = new Perber.Collection.ActivitysItems();
+
     // show main and speak pannel
     App.mainRegion.show(
-        new Perber.View.Speakpanel()
-    )
-
+        // new Perber.View.Speakpanel()
+        new Perber.View.Activity({
+            // new Perber.View.PerberItem()
+            collection: Perber.activity,
+            user : Perber.user
+        })
+    );
 
     App.headerRegion.show(
         new Perber.View.Navbar()
-    )
+    );
 
-    App.toolbarRegion.show(
-        new Perber.View.Toolbar()
-    )
+    // App.toolbarRegion.show(
+    //     new Perber.View.Toolbar(),
+    // );
+
     App.sidebarRegion.show(
         new Perber.View.Sidebar()
-    )
+    );
+
+
 });
 
 $(function(){
