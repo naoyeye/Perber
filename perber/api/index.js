0a882e9c35a865839fca38ff14d6e0b450f266d6
J.Y Han <ihanjiyun@gmail.com>
2015-02-09 13:07:04 +0800

    add new mailsender api for doraemoin2014

--- perber/api/index.js
+++ perber/api/index.js
@@ -2,12 +2,13 @@
 /* 
 * @Author: hanjiyun
 * @Date:   2014-05-06 14:57:44
-* @Last Modified by:   hanjiyun
-* @Last Modified time: 2014-05-07 21:14:27
+* @Last Modified by:   Jiyun
+* @Last Modified time: 2015-02-09 13:05:28
 */
 
 
 module.exports = API;
+var nodemailer = require("nodemailer");
 
 function API (app) {
     console.log('====== API service start ======');
@@ -71,9 +72,63 @@ function API (app) {
             }
 
         });
-
     });
 
+    // for wandoujia doraemon --- 2015-02-09
+    // push mail notification for third-party applications
+    var pond = [];
+    app.post('/api/v1/doraemonpushmail', function(req, res, next) {
+
+        // 如果池子里已经有了这个条目，就不再发送邮件
+        for (var i = pond.length - 1; i >= 0; i--) {
+            if (pond[i] === req.body.id) {
+                console.log('this id already exists');
+                return;
+            }
+        };
+
+        var config = app.get('config');
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+
+        console.log(req.body);
+        
+        var smtpTransport = nodemailer.createTransport("SMTP",{
+            service: "Gmail",
+            auth: {
+                user: config.mailer.user,
+                pass: config.mailer.pass
+            }
+        });
+
+        if (!req.body.type || !req.body.id) {
+            return;
+        }
+
+        var mail_text = '出现错误的条目是： ' + req.body.type + ' : ' + req.body.id + ' ，请尽快在 http://www.wandoujia.com/needle/ 中处理。这是一封自动发送的邮件，不要回答！！不要回答！！';
+
+        // setup e-mail data with unicode symbols
+        var mailOptions = {
+            from: "无聊治愈所的一号门神 ✔ <"+ config.mailer.user +">", // sender address
+            to: ['hanjiyun@wandoujia.com'], // list of receivers
+            subject: "T_T 无聊治愈所的条目又出问题了", // Subject line
+            text: mail_text//, // plaintext body
+        }
+
+        // send mail with defined transport object
+        smtpTransport.sendMail(mailOptions, function(error, response){
+            if(error){
+                console.log(error);
+                res.json({status : 'error'})
+            }else{
+                console.log("Mail sent: " + response.message);
+                pond.push(req.body.id);
+                res.json({status : 'success'})
+            }
+
+            // if you don't want to use this transport object anymore, uncomment following line
+            //smtpTransport.close(); // shut down the connection pool, no more messages
+        });
+    });
 
 }
 
